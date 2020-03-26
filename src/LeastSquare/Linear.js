24fbb9dafd455ddc976a4935acc07a4e77e1d4f7
6004062630485meen <62662854+6004062630485meen@users.noreply.github.com>
2020-03-27 01:04:40 +0700

    Update Linear.js

--- src/LeastSquare/Linear.js
+++ src/LeastSquare/Linear.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default () => (
   <div className="has-text-centered">
     <section class="hero is-primary">
-      <div className="container">
+      <div style={{ padding: 24, minHeight: 360, overflowX: 'auto' }}>
         <h1 className="title">Linear Regression</h1>
       </div>
     </section>
